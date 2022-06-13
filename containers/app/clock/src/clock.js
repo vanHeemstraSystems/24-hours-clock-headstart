baa2a0d58b8054409aea09c3a900a2f5114d0508
wvanheemstra <wvanheemstra@users.noreply.github.com>
2022-06-13 11:27:52 +0200

    Update clock.js

--- containers/app/clock/src/clock.js
+++ containers/app/clock/src/clock.js
@@ -1,93 +1,93 @@
-//create a canvas object fro HTML element
-var canvas = document.getElementById('canvas');
-//create a 2d drawing object
-var ctx = canvas.getContext('2d');
-//calculate the clock radius by using the height
-var radius = canvas.height / 2;
-//remap the x and y axis to the center of the canvas
-ctx.translate(radius, radius);
-//reduce clock radius by 90%
-radius = radius * 0.90;
+var spinner = document.querySelector("#spinner");
+var items = spinner.childNodes;
+var angle = 0;
+var img = new Image;
 
-setInterval(drawClock, 1000); //run the drawClock function every second.
+const clocks = [
+    {"day": "Sunday", "start": -2.1,"activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "olive"}]},
+    {"day": "Monday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "purple"}]},
+    {"day": "Tuesday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "blue"}]},
+    {"day": "Wednesday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "red"}]},
+    {"day": "Thursday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "pink"}]},
+    {"day": "Friday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 120, "color": "yellow"}]},
+    {"day": "Saturday", "start": -2.1, "activities": [{"name": "Sleep", "minutes": 83, "color": "darkblue"}, {"name": "bbb", "minutes": 140, "color": "green"}]}
+  ];
 
-function drawClock(){
-    drawFace(ctx, radius);
-    drawNumbers(ctx, radius);
-    drawTime(ctx, radius);
+var totalSpinnerItems = clocks.length;
+
+img.onload = function(){
+  console.log('inside img.onload');
+  for (var i=0;i<totalSpinnerItems;++i){  
+	console.log('inside for loop');
+	const number = i + 1;
+	console.log('number: ', number);
+    // Find the nth div, or create it
+	var div = items[i] || spinner.appendChild(document.createElement('div'));
+	console.log('div: ', div);
+    // Find the nth canvas, or create it
+    var canvas = div.getElementsByTagName('canvas')[0] ||
+                 div.appendChild(document.createElement('canvas'));
+	canvas.width  = 1; // Erase the canvas, in case it existed
+    canvas.width  = 300; // Set the width and height as desired
+    canvas.height = 300;			 
+	canvas.id = "spinnerItem-"+ number;
+	canvas.className = "spinnerItem";
+	var ctx = canvas.getContext('2d');
+	
+	// Added
+	var radius = canvas.height / 2;
+	const clock = clocks[i]
+	drawClock(ctx, radius, canvas, clock);
+	
+    // Use your actual calculations for the SVG size/position here
+    ctx.drawImage(img, 0, 0);
+  }
+}	
+
+// Be sure to set your image source after your load handler is in place
+img.src = "blank.svg";
+
+function drawClock(ctx, radius, canvas, clock) {
+	console.log('drawClock called');
+	drawFace(ctx, radius, canvas);
+	const start = clock.start;
+	const slices = clock.activities;
+	drawSlices(ctx, radius, start, slices);    	
 }
 
-function drawFace(ctx, radius){
-    var grad;
+function drawFace(ctx, radius, canvas) {
+	console.log('drawFace called');
+    var grad;	
     //draw white circle for the face
     ctx.beginPath();
-    ctx.arc(0,0,radius,0,2*Math.PI);
+	console.log('canvas.width: ', canvas.width);
+	console.log('canvas.height: ', canvas.height);
+    ctx.arc(canvas.width/2, canvas.height/2, radius*0.95, 0, 2*Math.PI);	
     ctx.fillStyle = "White";
     ctx.fill();
     // create a radial gradient (inner, middle, and outer edge of clock)
-    grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+    grad = ctx.createRadialGradient(canvas.width/2, canvas.height/2, radius*0.90, canvas.width/2, canvas.height/2, radius*1.0);
     grad.addColorStop(0, '#333');
     grad.addColorStop(0.5, 'white');
     grad.addColorStop(1, '#333');
     //define gradient as stroke style
     ctx.strokeStyle = grad;
     ctx.lineWidth = radius*0.1;
-    ctx.stroke(); 
+    ctx.stroke(); 	
     //draw the center of the clock
     ctx.beginPath();
-    ctx.arc(0,0, radius*0.1,0,2*Math.PI);
+    ctx.arc(canvas.width/2, canvas.height/2, radius*0.1, 0, 2*Math.PI);
     ctx.fillStyle = '#333';
     ctx.fill();
 }
 
-function drawNumbers(ctx, radius) {
-    var ang;
-    var num;
-    ctx.font = radius*0.15 + "px arial"; //set font at 15% of radius
-    ctx.textBaseline = "middle"; //set text alignment to middle
-    ctx.textAlign = "center"; //set text alignment to center
-    for(num=1; num < 13; num++){ //calculate the print position for each number
-        ang = num *Math.PI /6;
-        ctx.rotate(ang);
-        ctx.translate(0, -radius*0.85);
-        ctx.rotate(-ang);
-        ctx.fillText(num.toString(), 0, 0);
-        ctx.rotate(ang);
-        ctx.translate(0, radius*0.85);
-        ctx.rotate(-ang);
-    }
+function drawSlices() {
+	console.log('drawSlices called');	
+	// more ...
 }
 
-function drawTime(ctx, radius){
-    var now = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    //hour
-    hour = hour%12;
-    //calculate angle of hour hand
-    hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
-    //make hour hand 50% of canvas's radius
-    drawHand(ctx, hour, radius*0.5, radius*0.07);
-    //minute
-    //calculate angle of minute hand
-    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-    //make minute hand 80% of canvas's radius
-    drawHand(ctx, minute, radius*0.8, radius*0.07);
-    //second
-    //calculate angle of second hand
-    second=(second*Math.PI/30);
-    //make second hand 90% of canvas's radius
-    drawHand(ctx, second, radius*0.9, radius*0.02);
-}
-
-function drawHand(ctx, pos, length, width){
-    ctx.beginPath();
-    ctx.lineWidth = width;
-    ctx.lineCap = "round";
-    ctx.moveTo(0,0);
-    ctx.rotate(pos);
-    ctx.lineTo(0, -length);
-    ctx.stroke();
-    ctx.rotate(-pos);
+function galleryspin(sign) { 
+  spinner = document.querySelector("#spinner");
+  if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+  spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
